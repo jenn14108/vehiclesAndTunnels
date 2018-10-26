575c14c31444d8be17716ea24a00a6e28ac1016c
jenn14108 <jenn.lee14108@gmail.com>
2018-10-26 16:35:41 -0400

    Implement BasicTunnel and Factory

--- src/cs131/pa2/CarsTunnels/BasicTunnel.java
+++ src/cs131/pa2/CarsTunnels/BasicTunnel.java
@@ -2,21 +2,60 @@ package cs131.pa2.CarsTunnels;
 
 import cs131.pa2.Abstract.Tunnel;
 import cs131.pa2.Abstract.Vehicle;
+import cs131.pa2.Abstract.Direction;
 
 public class BasicTunnel extends Tunnel{
 
+	protected Direction tunnelDirection; //needed to keep track of the current direction of the one road in tunnel 
+	protected int sleds; //needed to keep track of the number of sleds in the tunnel. Should never exceed 1
+	protected int cars; //needed to keep track of the number of cars in the tunnel. Should never exceed 3
+	
+	
 	public BasicTunnel(String name) {
 		super(name);
+		this.sleds = 0;
+		this.cars = 0;
+		this.tunnelDirection = null; //no direction is determined yet when tunnel is first created
 	}
 
 	@Override
-	public boolean tryToEnterInner(Vehicle vehicle) {
-		return false;
+	public synchronized boolean tryToEnterInner(Vehicle vehicle) {
+		//first check what type of vehicle this is
+		//vehicle = car, then we need to check that no sled is inside, and the #
+		//of cars is < 3
+		if (vehicle instanceof Car) {
+			if (this.sleds > 0 || this.cars == 3) {
+				return false;
+			} else if (tunnelDirection == null || tunnelDirection.equals(vehicle.getDirection())){
+				this.cars ++;
+				this.tunnelDirection = vehicle.getDirection();
+				return true;
+			} else {
+				return false;
+			}
+		} else { //vehicle is an instance of Sled. We check if there is already a sled, or there is a car
+			if (this.sleds > 0 || this.cars > 0) {
+				return false;
+			} else if (tunnelDirection == null || tunnelDirection.equals(vehicle.getDirection())){
+				this.sleds ++;
+				this.tunnelDirection = vehicle.getDirection();
+				return true;
+			} else {
+				return false;
+			}
+		}
 	}
 
 	@Override
-	public void exitTunnelInner(Vehicle vehicle) {
-		
+	public synchronized void exitTunnelInner(Vehicle vehicle) {
+		if (vehicle instanceof Car) {
+			this.cars --;
+		} else {
+			this.sleds --;
+		}
+		if (this.cars == 0 && this.sleds == 0) {
+			this.tunnelDirection = null;
+		}
 	}
 	
 }
--- src/cs131/pa2/CarsTunnels/ConcreteFactory.java
+++ src/cs131/pa2/CarsTunnels/ConcreteFactory.java
@@ -12,17 +12,17 @@ public class ConcreteFactory implements Factory {
 
     @Override
     public Tunnel createNewBasicTunnel(String label){
-    		throw new UnsupportedOperationException("Not supported yet.");    
+    		return new BasicTunnel(label);
     }
 
     @Override
     public Vehicle createNewCar(String label, Direction direction){
-    		throw new UnsupportedOperationException("Not supported yet.");    
+    		return new Car(label, direction); 
     }
 
     @Override
     public Vehicle createNewSled(String label, Direction direction){
-    		throw new UnsupportedOperationException("Not supported yet.");    
+    		return new Sled(label,direction);   
     }
 
     @Override
