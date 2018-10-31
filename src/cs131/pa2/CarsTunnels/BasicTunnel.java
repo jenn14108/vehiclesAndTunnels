678f5d0a5aab105ea631d9f0f65eed3a574e5be0
jenn14108 <jenn.lee14108@gmail.com>
2018-10-30 23:26:57 -0400

    List of cars and Direction as is

--- src/cs131/pa2/CarsTunnels/BasicTunnel.java
+++ src/cs131/pa2/CarsTunnels/BasicTunnel.java
@@ -7,16 +7,17 @@ import java.util.*;
 
 public class BasicTunnel extends Tunnel{
 
-	protected List<Direction> tunnelDirection; //needed to keep track of the current direction of the one road in tunnel, and the vehicles inside of it 
+	protected Direction tunnelDirection; //needed to keep track of the current direction of the one road in tunnel 
 	protected int sleds; //needed to keep track of the number of sleds in the tunnel. Should never exceed 1
 	protected int cars; //needed to keep track of the number of cars in the tunnel. Should never exceed 3
-	
+	protected List<Vehicle> tunnelVehicles; //keep track of the vehicles in the tunnel in case needed in pt 2
 	
 	public BasicTunnel(String name) {
 		super(name);
 		this.sleds = 0;
 		this.cars = 0;
-		this.tunnelDirection = new ArrayList<>(); //no direction is determined yet when tunnel is first created
+		this.tunnelDirection = null; //no direction is determined yet when tunnel is first created
+		this.tunnelVehicles = new ArrayList<>();
 	}
 
 	@Override
@@ -27,9 +28,10 @@ public class BasicTunnel extends Tunnel{
 		if (vehicle instanceof Car) {
 			if (this.sleds > 0 || this.cars == 3) {
 				return false;
-			} else if (this.tunnelDirection.size()==0 || tunnelDirection.contains(vehicle.getDirection())){
+			} else if (tunnelDirection == null || tunnelDirection.equals(vehicle.getDirection())){
 				this.cars ++;
-				this.tunnelDirection.add(vehicle.getDirection());
+				this.tunnelDirection = vehicle.getDirection();
+				this.tunnelVehicles.add(vehicle);
 				return true;
 			} else {
 				return false;
@@ -37,9 +39,10 @@ public class BasicTunnel extends Tunnel{
 		} else { //vehicle is an instance of Sled. We check if there is already a sled, or there is a car
 			if (this.sleds > 0 || this.cars > 0) {
 				return false;
-			} else if (this.tunnelDirection.size()==0|| tunnelDirection.contains(vehicle.getDirection())){
+			} else if (tunnelDirection == null || tunnelDirection.equals(vehicle.getDirection())){
 				this.sleds ++;
-				this.tunnelDirection.add(vehicle.getDirection());
+				this.tunnelDirection = vehicle.getDirection();
+				this.tunnelVehicles.add(vehicle);
 				return true;
 			} else {
 				return false;
@@ -51,11 +54,13 @@ public class BasicTunnel extends Tunnel{
 	public synchronized void exitTunnelInner(Vehicle vehicle) {
 		if (vehicle instanceof Car) {
 			this.cars --;
+			this.tunnelVehicles.remove(vehicle);
 		} else {
 			this.sleds --;
+			this.tunnelVehicles.remove(vehicle);
 		}
 		if (this.cars == 0 && this.sleds == 0) {
-			this.tunnelDirection.remove(vehicle.getDirection());
+			this.tunnelDirection = null;
 		}
 	}
 	
